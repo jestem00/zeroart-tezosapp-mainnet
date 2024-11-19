863a653daeb784e421d742d960ec86348b2fecb6
jams2blues <jams2blues@gmail.com>
2024-11-18 22:24:27 -0600

    Finale

--- src/config/networkConfig.js
+++ src/config/networkConfig.js
@@ -5,7 +5,7 @@ import { NetworkType } from '@airgap/beacon-sdk';
 export const NETWORKS = {
   mainnet: {
     name: 'mainnet',
-    rpcUrl: process.env.REACT_APP_TEZOS_MAINNET_RPC || 'https://rpc.tzbeta.net/',
+    rpcUrl: process.env.REACT_APP_TEZOS_MAINNET_RPC || 'https://mainnet.ecadinfra.com',
     type: NetworkType.MAINNET,
   },
 };
